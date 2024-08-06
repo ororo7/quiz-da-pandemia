6c795a392f832c89023426d2e0381d73f91caa14
ororo7 <128612640+ororo7@users.noreply.github.com>
2024-08-06 12:07:53 -0300

    çkjfvf

--- script.js
+++ script.js
@@ -48,3 +48,19 @@ const perguntas = [
     },
       
 ];
+let atual=0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+      const botaoAlternativas = document.createElement("button");
+      botaoAlternativas.textContent = alternativa;
+      caixaAlternativas.appendChild(botaoAlternativas);
+    }
+  }
+mostraPergunta();
