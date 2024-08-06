7ddbe6c456884785d661ea1d6c6254e9988cdc31
ororo7 <128612640+ororo7@users.noreply.github.com>
2024-08-06 12:07:20 -0300

    kfhiu

--- script.js
+++ script.js
@@ -8,41 +8,41 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "PERGUNTA 1",
+        enunciado: "1-Você jogaria pelo Brasil no Mundial?",
         alternativas: [
             {
-                texto: "RESPOSTA 1!",
-                afirmacao: "AFIRMATIVA DA RESPOSTA 1r. "
+                texto: "sim, gostaria de jogar pelo Brasil, porquê os jogadores me motivam a continuar lutando pelo meu sonho.Tenho disposição e treino de 3 á 4 vezes por semana",
+                afirmacao: "Você irá jogar pelo Brasil no próximo mundial. "
             },
             {
-                texto: "RESPOSTA 2",
-                afirmacao: "AFIRMATIVA RESPOSTA 2."
+                texto: "Acho legal ter a opção de jogar pelo Brasil, mas pratico o esporte por diversão.Não me interesso pelos treinos que são mais pesados",
+                afirmacao: "você optou por não jogar pelo Brasil."
             }
         ]
     },
     {
-        enunciado: "PERGUNTA 2",
+        enunciado: "Você prefere assistir ou praticar vôlei?",
         alternativas: [
             {
-                texto: "RESPOSTA 1",
-                afirmacao: "AFIRMATIVA RESPOSTA 1"
+                texto: "não gosto de praticar esportes, pois acabo ficando de lado por não saber jogar.Logo, prefiro assistir os treinos e partidas de vôlei",
+                afirmacao: "você preferiu assistir "
             },
             {
-                texto: "RESPOSTA 2",
-                afirmacao: "AFIRMATIVA RESPOSTA 2"
+                texto:"praticar, pois vôlei é a minha modalidade esportiva favorita"
+                afirmacao: "Você preferiu jogar"
             }
         ]
     },
     {
-        enunciado: "PERGUNTA 3",
+        enunciado: " Prefetre jogar como Central ou Levantdor ?",
         alternativas: [
             {
-                texto: "RESPOSTA 1",
-                afirmacao: "AFIRMATIVA RESPOSTA 1"
+                texto: "como Central, pois brilho mais que os outros jogadores e consigo pular mais alto e dar meu melhor.",
+                afirmacao: "Você será o central do time."
             },
             {
-                texto: "RESPOSTA 2",
-                afirmacao: "AFIRMATIVA RESPOSTA 2."
+                texto: "como levantador porque, consigo distribuir mais as jogadas para meu time e faze-lo ficar 100% conectados um com o outro .",
+                afirmacao: "Você será o levanatdor do time."
             }
         ]
     },
