b4ed226914e5d767c2a059cc28532081e4c0e885
ororo7 <128612640+ororo7@users.noreply.github.com>
2024-08-27 11:11:45 -0300

    ok

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const perguntas = [
                 afirmacao: "você preferiu assistir "
             },
             {
-                texto:"praticar, pois vôlei é a minha modalidade esportiva favorita"
+                texto:"praticar, pois vôlei é a minha modalidade esportiva favorita",
                 afirmacao: "Você preferiu jogar"
             }
         ]
@@ -48,19 +48,42 @@ const perguntas = [
     },
       
 ];
-let atual=0;
+
+let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
-function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
-      const botaoAlternativas = document.createElement("button");
-      botaoAlternativas.textContent = alternativa;
-      caixaAlternativas.appendChild(botaoAlternativas);
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
-  }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = " Um ano depois...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
 mostraPergunta();
